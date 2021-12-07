edd628e2e295a7cb863cb42c3815aead2010b652
Christian.Nyabeye <christian.nyabeyengatchou@bosa.fgov.be>
2021-12-07 21:21:20 +0100

    add even sourcing

---
+++ src/main/java/be/tobania/demo/kafka/orderService/config/OrderServiceConsumerConfig.java
@@ -0,0 +1,4 @@
+package be.tobania.demo.kafka.orderService.config;
+
+public class OrderServiceConsumerConfig {
+}
---
+++ src/main/java/be/tobania/demo/kafka/orderService/model/enums/PaymentStatus.java
@@ -0,0 +1,32 @@
+package be.tobania.demo.kafka.orderService.model.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum PaymentStatus {
+    PLACED("placed"),
+
+    ONGOING("ongoing"),
+
+    PAYED("payed");
+
+    private String value;
+
+    PaymentStatus(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+    public static PaymentStatus fromValue(String value) {
+        for (PaymentStatus b : PaymentStatus.values()) {
+            if (b.value.equals(value)) {
+                return b;
+            }
+        }
+        throw new IllegalArgumentException("Unexpected value '" + value + "'");
+    }
+
+}
